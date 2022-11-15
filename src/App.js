4bdde54cd5fcf8be0a38f004735994f1b6af3c5e
Nikolaus Matten <nfmatten@gmail.com>
2022-11-15 16:08:53 -0700

    feat: Created function to fetch data from database and instantiated components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import NavBar from "./Components/NavBar/NavBar";
+import DisplayMovies from "./Components/DisplayMovies/DisplayMovies";
+import { Box } from "@mui/material";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  const fetchMovies = async (query) => {
+    try {
+      const response = await axios.get("http://localhost:8080/movies");
+      setMovies(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Box sx={{ my: 5 }}>
+        <DisplayMovies movies={movies} />
+      </Box>
     </div>
   );
 }
