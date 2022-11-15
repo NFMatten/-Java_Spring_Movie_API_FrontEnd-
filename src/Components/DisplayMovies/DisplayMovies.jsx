dd973abc800da38ff9026480a2b8ec34f851aa3e
Nikolaus Matten <nfmatten@gmail.com>
2022-11-15 16:12:20 -0700

    feat: Created DisplayMovies Component to display movies retreived from the database.

---
+++ src/Components/DisplayMovies/DisplayMovies.jsx
@@ -0,0 +1,37 @@
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import { Container, Grid } from "@mui/material";
+
+const DisplayMovies = (props) => {
+  const { movies } = props;
+
+  return (
+    <Container>
+      <Grid container rowSpacing={2} columnSpacing={3}>
+        {movies.map((movie) => (
+          <Grid item xs={12} sm={6} md={4} key={`${movie.id}`}>
+            <Card sx={{ maxWidth: 375, maxHeight: 600 }}>
+              <CardHeader
+                action={<IconButton aria-label="settings"></IconButton>}
+                title={movie.name}
+                subheader={`${movie.director} - ${movie.genre} `}
+              />
+              <CardMedia
+                padding="1em"
+                component="img"
+                height="600"
+                image={`${movie.imgUrl}`}
+                alt="Paella dish"
+              />
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default DisplayMovies;
