6ccc051916d31947286564be687138a66bccdd58
Nikolaus Matten <nfmatten@gmail.com>
2022-11-16 10:36:37 -0700

    refactor: created popover to navbar + style: updated styling to navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,11 +3,21 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import Popper from "@mui/material/Popper";
+import "./NavBar.css";
 
 export default function ButtonAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popper" : undefined;
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -18,13 +28,42 @@ export default function ButtonAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={handleClick}
           >
             <MenuIcon />
           </IconButton>
+          <Popper id={id} open={open} anchorEl={anchorEl}>
+            <Box
+              sx={{
+                p: 1,
+                bgcolor: "#1976d2",
+                ml: 1,
+                minHeight: 75,
+              }}
+            >
+              <ul className="ul-style">
+                <li className="li-style">
+                  <a
+                    className="popover-link"
+                    href="https://github.com/NFMatten/Java_Spring_Movie_API_FrontEnd"
+                  >
+                    Movie Library Frontend
+                  </a>
+                </li>
+                <li className="li-style">
+                  <a
+                    className="popover-link"
+                    href="https://github.com/NFMatten/Java_Spring_Movie_API"
+                  >
+                    Movie Library Backend
+                  </a>
+                </li>
+              </ul>
+            </Box>
+          </Popper>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Movie Library
           </Typography>
-          <Button color="inherit">Add New Movie</Button>
         </Toolbar>
       </AppBar>
     </Box>
