c46886a8737c45df6bf589498169b08d5b466d58
Nikolaus Matten <nfmatten@gmail.com>
2022-11-16 10:38:18 -0700

    feat: Created functional Add New Movie form to component

--- src/Components/CreateMovieForm/CreateMovieForm.jsx
+++ src/Components/CreateMovieForm/CreateMovieForm.jsx
@@ -1,21 +1,70 @@
+import { Container, Stack, Button } from "@mui/material";
 import { TextField } from "@mui/material";
 import React, { useState } from "react";
-import useCustomForm from "../../hooks/useCustomForm";
 
 const CreateMovieForm = (props) => {
   const [name, setName] = useState("");
   const [genre, setGenre] = useState("");
   const [director, setDirector] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let newMovie = {
+      name: name,
+      genre: genre,
+      director: director,
+      imgUrl: imgUrl,
+    };
+    props.createNewMovie(newMovie);
+  }
 
   return (
     <div>
-      <form className="form" onSubmit={handleSumbit}>
-        <TextField
-          label="Movie Title"
-          placeholder="Enter Movie Title"
-          type={text}
-          name="name"
-        />
+      <form onSubmit={handleSubmit}>
+        <Container
+          maxWidth="xs"
+          sx={{ backgroundColor: "white", py: 3, borderRadius: 2 }}
+        >
+          <Stack spacing={2}>
+            <h2>Add New Movie</h2>
+            <TextField
+              label="Enter Movie Title"
+              placeholder="Movie Title"
+              type="text"
+              name="name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+            <TextField
+              label="Enter Movie Genre"
+              placeholder="Movie Genre"
+              type="text"
+              name="genre"
+              value={genre}
+              onChange={(event) => setGenre(event.target.value)}
+            />
+            <TextField
+              label="Enter Movie Director"
+              placeholder="Movie Director"
+              type="text"
+              name="director"
+              value={director}
+              onChange={(event) => setDirector(event.target.value)}
+            />
+            <TextField
+              label="Enter Movie Image URL"
+              placeholder="Movie Image URL"
+              type="text"
+              name="imgUrl"
+              value={imgUrl}
+              onChange={(event) => setImgUrl(event.target.value)}
+            />
+            <Button variant="contained" type="submit">
+              Add New Movie
+            </Button>
+          </Stack>
+        </Container>
       </form>
     </div>
   );
