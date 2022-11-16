8182d9a8dcf121e4c7be36abb8d077d35e6e5fc6
Nikolaus Matten <nfmatten@gmail.com>
2022-11-16 10:38:58 -0700

    feat: Created async function to create new movie and instantiated CreateMovieForm Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import NavBar from "./Components/NavBar/NavBar";
 import DisplayMovies from "./Components/DisplayMovies/DisplayMovies";
 import { Box } from "@mui/material";
+import CreateMovieForm from "./Components/CreateMovieForm/CreateMovieForm";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -20,10 +21,25 @@ function App() {
     }
   };
 
+  const createNewMovie = async (newMovie) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/addMovie",
+        newMovie
+      );
+      if (response.status === 201) await fetchMovies();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <NavBar />
       <Box sx={{ my: 5 }}>
+        <Box sx={{ my: 2 }}>
+          <CreateMovieForm createNewMovie={createNewMovie} />
+        </Box>
         <DisplayMovies movies={movies} />
       </Box>
     </div>
